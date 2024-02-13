b97fbb2df19c3783d6145b46561c6ae80a8beacc
Minin <70429604+MininxD@users.noreply.github.com>
2024-02-13 15:25:10 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
+import {gif} from './randomGif.js';
+import {increment} from './autoList.js';
+import {randColor, stopRandColor} from './color.js';
 
+  increment();
+
+var lagu = new Audio("../konga.mp3");
+gsap.registerPlugin(Draggable);
+
+export var imageDrag = Draggable.create("#imageDiv", {  
+  bounds: ".konten",
+  autoScroll:1
+})[0];
+
+
+imageDrag.addEventListener("click", function() {
+  increment();
+  gif();
+  konten.classList.add("animated");
+  kontainer.classList.add("animated");
+  overlay.style.display = "block";
+  lagu.play();
+});
+
+
+
+
+
+imageDiv.addEventListener("touchstart", function() {
+  increment();
+  konten.classList.add("animated");
+  kontainer.classList.add("animated");
+  overlay.style.display = "block";
+  lagu.play();
+})
+imageDiv.addEventListener("mousedown", function() {
+  increment();
+  konten.classList.add("animated");
+  kontainer.classList.add("animated");
+  overlay.style.display = "block";
+  lagu.play();
+})
+imageDiv.addEventListener("touchstart", function() {
+  randColor();
+})
+imageDiv.addEventListener("mousedown", function() {
+  randColor();
+})
+imageDiv.addEventListener("touchend", function() {
+  stopRandColor();
+})
+imageDiv.addEventListener("mousedown", function() {
+  stopRandColor();
+})
